0c3b2a8ff4b6fc3ba9e9d10168a498bb73207982
mirzanahid <mirzasaikat3@gmail.com>
2025-02-24 11:24:53 +0600

    chages 7

--- api/bot.js
+++ api/bot.js
@@ -4,10 +4,7 @@ const mongoose = require("mongoose");
 const cron = require("node-cron");
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log("MongoDB connected successfully"))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connected successfully"))
   .catch((err) => {
     console.error("MongoDB connection error:", err);
     process.exit(1); // Exit the process if the connection fails
