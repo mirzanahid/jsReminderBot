348314c50f83d45e3c6b536288e1db7802827700
mirzanahid <mirzasaikat3@gmail.com>
2025-02-24 11:37:21 +0600

    changes 9

--- api/bot.js
+++ api/bot.js
@@ -7,8 +7,6 @@ const cron = require("node-cron");
 const connectToMongo = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 30000,
     });
     console.log('MongoDB connected successfully');
